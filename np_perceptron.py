d81154dc1be706aa76b0376703536bc5e673123d
eyewheel <185627573+eyewheel@users.noreply.github.com>
2024-11-15 18:23:02 -0700

    currently classifies entirely randomly

--- np_perceptron.py
+++ np_perceptron.py
@@ -6,9 +6,18 @@ def mcculloch_pitts_neuron(inputs, weights, threshold):
     return np.sum(np.dot(inputs, weights)) > threshold
 
 class Perceptron:
-    def __init__(self, size):
+    def __init__(self, size, threshold):
         self.weights = np.random.rand(size)
+        self.threshold = threshold
 
-p = Perceptron()
-print(p.weights)
+    def activation(self, result):
+        return result > self.threshold
 
+    def classify(self, inputs):
+        if inputs.shape != self.weights.shape:
+            raise ValueError(f"inputs must be a 1d array of size {self.weights.shape[0]}")
+        return self.activation(np.sum(np.dot(inputs, self.weights)))
+
+p = Perceptron(2, 0)
+
+print(p.classify(np.array([0, 1])))
