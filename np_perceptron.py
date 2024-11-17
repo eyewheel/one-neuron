201c3db5f8118d84b880dde86d8e851ca022d264
eyewheel <185627573+eyewheel@users.noreply.github.com>
2024-11-16 19:04:23 -0700

    multiple neuron perceptron

--- np_perceptron.py
+++ np_perceptron.py
@@ -5,7 +5,7 @@ import numpy as np
 def mcculloch_pitts_neuron(inputs, weights, threshold):
     return np.sum(np.dot(inputs, weights)) > threshold
 
-class Perceptron:
+class PerceptronNeuron:
     def __init__(self, size, threshold):
         self.weights = np.random.rand(size)
         self.threshold = threshold
@@ -27,7 +27,39 @@ class Perceptron:
         self.weights += self.learning_rate * self.loss(expected, actual) * inputs
         return self.classify(inputs)
 
-p = Perceptron(2, 0)
+class Perceptron:
+    def __init__(self, size, neurons, threshold):
+        self.weights = np.random.rand(neurons, size)
+        print(self.weights)
+        self.threshold = threshold
+        self.learning_rate = 0.2
+
+    def classify(self, inputs):
+        # run activation on the dot product of the inputs with each neuron
+        return self.activation(np.matmul(self.weights, inputs))
+
+    def activation(self, results):
+        return np.where(results > self.threshold, 1, 0)
+
+    def loss(self, expected, actual):
+        return expected - actual
+
+    def train(self, inputs, expected):
+        actual = self.classify(inputs)
+
+        # currently we have two vectors of different sizes - the input
+        # will be of size (size,), while the loss will be of size (neurons,)
+        # (1, size)
+        inputs = inputs.reshape(1, -1)
+        # (neurons, 1)
+        loss = self.loss(expected, actual).reshape(-1, 1)
+        # numpy broadcasting will create a matrix where each row is the
+        # loss across inputs for a given neuron.
+        # each row in the weights is a neuron, so we can simply add
+        update_weights = (loss * inputs)
+        self.weights += update_weights
+
+p = Perceptron(2, 3, 0)
 
 print(p.classify(np.array([0, 1])))
 print(p.classify(np.array([1, 1])))
@@ -36,8 +68,13 @@ print(p.classify(np.array([1, 0])))
 print("begin...")
 
 while True:
-    print(p.train(np.array([0, 1]), 0))
-    print(p.train(np.array([1, 1]), 1))
-    print(p.train(np.array([0, 0]), 0))
-    print(p.train(np.array([1, 0]), 0))
+    p.train(np.array([0, 1]), 1)
+    p.train(np.array([1, 1]), 1)
+    p.train(np.array([0, 0]), 0)
+    p.train(np.array([1, 0]), 0)
+
+    print(p.classify(np.array([0, 1])))
+    print(p.classify(np.array([1, 1])))
+    print(p.classify(np.array([0, 0])))
+    print(p.classify(np.array([1, 0])))
     input(">")
