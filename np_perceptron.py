3314e2db68efa30ad146790ed86a2ea77c34d005
eyewheel <185627573+eyewheel@users.noreply.github.com>
2024-11-17 09:07:24 -0700

    another interesting misunderstanding

--- np_perceptron.py
+++ np_perceptron.py
@@ -27,53 +27,46 @@ class PerceptronNeuron:
         self.weights += self.learning_rate * self.loss(expected, actual) * inputs
         return self.classify(inputs)
 
-class Perceptron:
-    def __init__(self, size, neurons, threshold):
-        self.weights = np.random.rand(neurons, size)
-        self.threshold = threshold
+class PerceptronLayer:
+    def __init__(self, size, neurons, biases):
+        self.weights = np.random.rand(size + 1, neurons)
+        # biases should be a vector (neurons,)
+        if biases.shape[0] is not neurons:
+            raise ValueError("must have 1 bias per neuron")
+        self.biases = biases.reshape(-1, 1) # (1, neurons)
         self.learning_rate = 0.2
 
     def classify(self, inputs):
+        inputs = self.with_bias(inputs)
+        print(inputs)
         # run activation on the dot product of the inputs with each neuron
         return self.activation(np.matmul(self.weights, inputs))
 
+    def with_bias(self, inputs):
+        # turn inputs into a matrix stack of itself, one input per neuron
+        broadcaster = np.ones((self.weights.shape[0], 1))
+        inputs = broadcaster * inputs
+        # append biases as the last column
+        inputs = np.hstack((inputs, self.biases))
+        return inputs
+
     def activation(self, results):
-        return np.where(results > self.threshold, 1, 0)
+        return np.where(results > 0, 1, 0)
 
     def loss(self, expected, actual):
         return expected - actual
 
     def train(self, inputs, expected):
         actual = self.classify(inputs)
-
-        # currently we have two vectors of different sizes - the input
-        # will be of size (size,), while the loss will be of size (neurons,)
-        # (1, size)
-        inputs = inputs.reshape(1, -1)
-        # (neurons, 1)
-        loss = self.loss(expected, actual).reshape(-1, 1)
+        inputs = self.with_bias(inputs)
+        loss = self.loss(expected, actual)
         # numpy broadcasting will create a matrix where each row is the
         # loss across inputs for a given neuron.
-        # each row in the weights is a neuron, so we can simply add
         update_weights = (loss * inputs)
         self.weights += update_weights
 
-p = Perceptron(2, 3, 0)
+p = PerceptronLayer(2, 3, np.zeros(3))
 
-print(p.classify(np.array([0, 1])))
 print(p.classify(np.array([1, 1])))
-print(p.classify(np.array([0, 0])))
-print(p.classify(np.array([1, 0])))
-print("begin...")
 
-while True:
-    p.train(np.array([0, 1]), 1)
-    p.train(np.array([1, 1]), 1)
-    p.train(np.array([0, 0]), 0)
-    p.train(np.array([1, 0]), 0)
 
-    print(p.classify(np.array([0, 1])))
-    print(p.classify(np.array([1, 1])))
-    print(p.classify(np.array([0, 0])))
-    print(p.classify(np.array([1, 0])))
-    input(">")
